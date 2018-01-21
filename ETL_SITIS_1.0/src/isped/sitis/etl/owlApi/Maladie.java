dec90bffabf833fc825c13d5dc5f0a30e91c8d72
hadrien <hadrien.sentenac@gmail.com>
2018-01-21 21:41:22 +0100

    classe maladie

---
+++ ETL_SITIS_1.0/src/isped/sitis/etl/owlApi/Maladie.java
@@ -0,0 +1,27 @@
+package isped.sitis.etl.owlApi;
+
+public class Maladie {
+	
+	private String ID;
+	private String ClassEquilMal;
+	private String Superclass;
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getClassEquilMal() {
+		return ClassEquilMal;
+	}
+	public void setClassEquilMal(String classEquilMal) {
+		ClassEquilMal = classEquilMal;
+	}
+	public String getSuperclass() {
+		return Superclass;
+	}
+	public void setSuperclass(String superclass) {
+		Superclass = superclass;
+	}
+
+}
